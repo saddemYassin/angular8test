dc2d485fc88cd8c104e8baa880f2e7de0683cad1
saddem123 <saddem123@github.com>
2019-07-10 11:44:47 +0100

    add node server and express

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 import { Subscription } from 'rxjs';
+
 @Component({
     selector: 'app-post-list',
     templateUrl: './post-list.component.html',
