7dd4c546218031385c756706e5c73f96bf0d98b1
saddem123 <saddem123@github.com>
2019-07-10 16:01:22 +0100

    add post methode without DB

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+
 
 const app = express()
+
+app.use(bodyParser.json())
 
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin','*')
     res.setHeader(
-        'Access-Control-Allow-Header',
+        'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-type, Accept')
     res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS")    
     next()
 })
+app.post("/api/posts", (req,res,next) => {
+    const post = req.body;
+    console.log(post)
+    res.status(201).json({
+        message: "Post added successfully !"
+    })
+})
 
-app.use('/api/posts',(req, res, next)=> {
+app.get('/api/posts',(req, res, next)=> {
     const posts = [
         {id: '1',title:'First server-side post',content:'This is coming from server!'},
         {id: '2',title:'Second server-side post',content:'This is coming from server !'},
